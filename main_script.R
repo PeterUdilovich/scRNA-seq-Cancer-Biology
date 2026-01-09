ccaa1de1bd8d7c911059f7b5927f6a25faf7ed66
PeterUdilovich <47163832+PeterUdilovich@users.noreply.github.com>
2026-01-09 10:51:16 -0500

    Finished data preprocessing

--- main_script.R
+++ main_script.R
@@ -21,7 +21,7 @@ library(Seurat)
 library(patchwork)
 
 
-# Load the PBMC dataset
+# Load the datasets ####
 benign_breast.data <- Read10X_h5("/Users/test/Desktop/Compbio/main_rna_project/project_data/benign_breast/filtered_feature_bc_matrix.h5")
 tumor_breast.data <- Read10X(data.dir = "/Users/test/Desktop/Compbio/main_rna_project/project_data/malignant_breast_sample")
 benign_icc.data <- read.csv(
@@ -41,53 +41,118 @@ tumor_icc.data <- read.csv(
 tumor_icc.data <- as.matrix(tumor_icc.data)
 
 
-#Seurat Objects
+#Seurat Objects ####
 benign_breast <- CreateSeuratObject(counts = benign_breast.data, project = "bb", min.cells = 3, min.features = 200)
 tumor_breast <- CreateSeuratObject(counts = tumor_breast.data, project = "tb", min.cells = 3, min.features = 200)
 benign_icc <- CreateSeuratObject(counts = benign_icc.data, project = "bicc", min.cells = 3, min.features = 200)
 tumor_icc <- CreateSeuratObject(counts = tumor_icc.data, project = "ticc", min.cells = 3, min.features = 200)
 
+#Initial QC ####
 benign_breast[["percent.mt"]] <- PercentageFeatureSet(benign_breast, pattern = "^MT-")
 tumor_breast[["percent.mt"]] <- PercentageFeatureSet(tumor_breast, pattern = "^MT-")
 benign_icc[["percent.mt"]] <- PercentageFeatureSet(benign_icc, pattern = "^MT-")
 tumor_icc[["percent.mt"]] <- PercentageFeatureSet(tumor_icc, pattern = "^MT-")
 
+#Violin Plot
 VlnPlot(benign_breast, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
 VlnPlot(tumor_breast, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
 VlnPlot(benign_icc, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
 VlnPlot(tumor_icc, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
 
+#Scatter Plots
+plot1 <- FeatureScatter(benign_breast, feature1 = "nCount_RNA", feature2 = "percent.mt")
+plot2 <- FeatureScatter(benign_breast, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+plot1 + plot2
+plot1 <- FeatureScatter(tumor_breast, feature1 = "nCount_RNA", feature2 = "percent.mt")
+plot2 <- FeatureScatter(tumor_breast, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+plot1 + plot2
+plot1 <- FeatureScatter(benign_icc, feature1 = "nCount_RNA", feature2 = "percent.mt")
+plot2 <- FeatureScatter(benign_icc, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+plot1 + plot2
+plot1 <- FeatureScatter(tumor_icc, feature1 = "nCount_RNA", feature2 = "percent.mt")
+plot2 <- FeatureScatter(tumor_icc, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+plot1 + plot2
+
+#filter based on QC
 benign_breast <- subset(benign_breast, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
-benign_breast <- NormalizeData(benign_breast)
+tumor_breast <- subset(tumor_breast, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
+benign_icc <- subset(benign_icc, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
+tumor_icc <- subset(tumor_icc, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
 
+
+#Normalize the Data ####
+
+benign_breast <- NormalizeData(benign_breast, normalization.method = "LogNormalize", scale.factor = 10000)
+tumor_breast <- NormalizeData(tumor_breast, normalization.method = "LogNormalize", scale.factor = 10000)
+benign_icc <- NormalizeData(benign_icc, normalization.method = "LogNormalize", scale.factor = 10000)
+tumor_icc <- NormalizeData(tumor_icc, normalization.method = "LogNormalize", scale.factor = 10000)
+
+
+#Identification of highly variable features ####
 benign_breast <- FindVariableFeatures(benign_breast, selection.method = "vst", nfeatures = 2000)
-
-"""benign_breast <- FindVariableFeatures(benign_breast, selection.method = "vst", nfeatures = 2000)
+tumor_breast <- FindVariableFeatures(tumor_breast, selection.method = "vst", nfeatures = 2000)
+benign_icc <- FindVariableFeatures(benign_icc, selection.method = "vst", nfeatures = 2000)
+tumor_icc <- FindVariableFeatures(tumor_icc, selection.method = "vst", nfeatures = 2000)
 
 # Identify the 10 most highly variable genes
-top10 <- head(VariableFeatures(benign_breast), 10)
+bbtop10 <- head(VariableFeatures(benign_breast), 10)
+tbtop10 <- head(VariableFeatures(tumor_breast), 10)
+bicctop10 <- head(VariableFeatures(benign_icc), 10)
+ticctop10 <- head(VariableFeatures(tumor_icc), 10)
 
 # plot variable features with and without labels
 plot1 <- VariableFeaturePlot(benign_breast)
-plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
-plot1 + plot2"""
+plot2 <- LabelPoints(plot = plot1, points = bbtop10, repel = TRUE)
+plot1 + plot2
+plot1 <- VariableFeaturePlot(tumor_breast)
+plot2 <- LabelPoints(plot = plot1, points = tbtop10, repel = TRUE)
+plot1 + plot2
+plot1 <- VariableFeaturePlot(benign_icc)
+plot2 <- LabelPoints(plot = plot1, points = bicctop10, repel = TRUE)
+plot1 + plot2
+plot1 <- VariableFeaturePlot(tumor_icc)
+plot2 <- LabelPoints(plot = plot1, points = ticctop10, repel = TRUE)
+plot1 + plot2
 
+# Scaling the data ####
+bb_all.genes <- rownames(benign_breast)
+benign_breast <- ScaleData(benign_breast, features = bb_all.genes)
+tb_all.genes <- rownames(tumor_breast)
+tumor_breast <- ScaleData(tumor_breast, features = tb_all.genes)
+bicc_all.genes <- rownames(benign_icc)
+benign_icc <- ScaleData(benign_icc, features = bicc_all.genes)
+ticc_all.genes <- rownames(tumor_icc)
+tumor_icc <- ScaleData(tumor_icc, features = ticc_all.genes)
 
+head(tumor_icc[["RNA"]]$scale.data)
 
-all.genes <- rownames(benign_breast)
-benign_breast <- ScaleData(benign_breast, features = all.genes)
-
-
+#Perform linear dimensional reduction ####
 
 benign_breast <- RunPCA(benign_breast, features = VariableFeatures(object = benign_breast))
-print(benign_breast[["pca"]], dims = 1:5, nfeatures = 5)
-
-VizDimLoadings(benign_breast, dims = 1:2, reduction = "pca")
+tumor_breast <- RunPCA(tumor_breast, features = VariableFeatures(object = tumor_breast))
+benign_icc <- RunPCA(benign_icc, features = VariableFeatures(object = benign_icc))
+tumor_icc <- RunPCA(tumor_icc, features = VariableFeatures(object = tumor_icc))
 
 DimPlot(benign_breast, reduction = "pca") + NoLegend()
+DimPlot(tumor_breast, reduction = "pca") + NoLegend()
+DimPlot(benign_icc, reduction = "pca") + NoLegend()
+DimPlot(tumor_icc, reduction = "pca") + NoLegend()
 
-benign_breast <- RunUMAP(benign_breast, dims = 1:10)
-DimPlot(benign_breast, reduction = "umap")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
